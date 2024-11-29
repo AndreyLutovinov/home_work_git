947923e5a7056e7e9c09133923536571f624e65b
AndreyLutovinov <andreilute@gmail.com>
2024-11-29 23:37:15 +0300

    added getMassage() method

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -3,6 +3,9 @@ package guru.qa;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, AQA!");
+        System.out.println(getMassage());
+    }
+    private static String getMassage() {
+        return "Hello, AQA!";
     }
 }
